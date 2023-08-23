925d1c468acee455278e73a70f65e3c95293c85b
Tolu Adesina <tolu.adesina79@gmail.com>
2023-08-23 20:52:56 +0200

    ADD bad gateway class error handler

--- back-end/src/utils/error/error-handler.ts
+++ back-end/src/utils/error/error-handler.ts
@@ -1,16 +1,17 @@
 import { GraphQLError, GraphQLErrorOptions } from 'graphql';
-import { ReturnResponseStatus } from "../../helpers/response-handling";
+import { ReturnResponseMessage, ReturnResponseStatus } from "../../helpers/response-handling";
 
 /**
  * Internal Server Error Handler that extends the graphql GraphQL Error class.
  * 
  * Used to return and error if issues occurred when request is made.
+ * @constructor Requires message, to return back to user.
  */
 export class ApolloInternalServerError extends GraphQLError {
-  constructor(message: string) {
+  constructor(message: string = ReturnResponseMessage(500)) {
     const options: GraphQLErrorOptions = {
       extensions: {
-        code: ReturnResponseStatus(403),
+        code: ReturnResponseStatus(500),
       }
     };
 
@@ -22,9 +23,10 @@ export class ApolloInternalServerError extends GraphQLError {
  * Unauthorised Error Handler that extends the graphql GraphQL Error class.
  * 
  * Used to return and error if issues occurred when request is made.
+ * @constructor Requires message, to return back to user.
  */
 export class ApolloUnauthorisedError extends GraphQLError {
-  constructor(message: string) {
+  constructor(message: string = ReturnResponseMessage(401)) {
     const options: GraphQLErrorOptions = {
       extensions: {
         code: ReturnResponseStatus(401),
@@ -34,3 +36,21 @@ export class ApolloUnauthorisedError extends GraphQLError {
     super(message, options)
   }
 }
+
+/**
+ * Bad Gateway Error Handler that extends the graphql GraphQL Error class.
+ * 
+ * Used to return and error if issues occurred when request is made.
+ * @constructor Requires message, to return back to user.
+ */
+export class ApolloBadGatewayError extends GraphQLError {
+  constructor(message: string = ReturnResponseMessage(502)) {
+    const options: GraphQLErrorOptions = {
+      extensions: {
+        code: ReturnResponseStatus(502),
+      }
+    };
+
+    super(message, options)
+  }
+} 
